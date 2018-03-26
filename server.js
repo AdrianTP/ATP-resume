8c1edfff34c5b0edda8f378460cc0201f8c16e45
AdrianTP <github@adriantp.com>
2018-03-26 18:39:54 +0000

    Some small tweaks

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 //app.use(app.router);
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 app.use(gen.error.log);
 app.use(gen.error.response);
 app.use(gen.error.general);
